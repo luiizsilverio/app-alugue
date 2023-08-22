be50539f6adf589d46d57b4d158ef5b293e120c6
luiizsilverio <luiiz.silverio@gmail.com>
2023-08-22 19:19:43 -0300

    componente New, fotos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, ScrollView, View, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
+import New from '../components/New';
+
 export default function Home() {
+  const navigation = useNavigation();
+
   return (
     <ScrollView
       showsVerticalScrollIndicator={false}
@@ -16,6 +21,32 @@ export default function Home() {
         </View>
 
       </View>
+
+      <View style={styles.contentNew}>
+        <Text style={styles.title}>Novidades</Text>
+      </View>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <New
+          cover={require('../assets/house1.jpg')}
+          name="Casa de praia"
+          description="Casa nova a uma quadra da praia, lugar seguro e monitorado 24 hs"
+          onPress={() => navigation.navigate('details')}
+        />
+        <New
+          cover={require('../assets/house2.jpg')}
+          name="Casa de praia"
+          description="Casa nova a uma quadra da praia, lugar seguro e monitorado 24 hs"
+          onPress={() => navigation.navigate('details')}
+        />
+        <New
+          cover={require('../assets/house3.jpg')}
+          name="Casa de praia"
+          description="Casa nova a uma quadra da praia, lugar seguro e monitorado 24 hs"
+          onPress={() => navigation.navigate('details')}
+        />
+      </ScrollView>
+
     </ScrollView>
   )
 }
@@ -25,6 +56,35 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    width: '100%',
+    marginVertical: 20,
+  },
+  inputArea: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '98%',
+    backgroundColor: '#FFF',
+    elevation: 2,
+    paddingHorizontal: 10,
+    height: 37,
+    borderRadius: 10,
+  },
+  input: {
+    fontFamily: 'Montserrat-Medium',
+    paddingHorizontal: 10,
+    fontSize: 13,
+    width: '90%'
+  },
+  contentNew: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center'
+  },
+  title: {
+    paddingHorizontal: 15,
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 18,
+    color: '#4f4a4a'
   }
 });
